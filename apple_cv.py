788dfd099768e0f4db68429a5e94e43a0b9d2375
당근철봉 <eunhocho@Eunhoui-MacBookPro.local>
2021-08-22 16:54:16 +0900

    file indent fix

--- apple_cv.py
+++ apple_cv.py
@@ -1,33 +1,36 @@
-from PIL import ImageGrab
 import cv2 as cv
 import numpy as np
+# from PIL import ImageGrab
 
-def find_min_max_coordinate(find_coord, axis):
-    min, max, coord = 0, 0, 0
+
+def find_min_max_coordinate(find_coord_list, axis):
+    res_min, res_max, coord = 0, 0, 0
     if axis == 'y':
         coord = 1
-    min = find_coord[0][coord]
-    max = find_coord[0][coord] + find_coord[0][coord + 2]
-    for i in range(len(find_coord)):
-        f_min = find_coord[i][coord]
-        f_max = find_coord[i][coord] + find_coord[i][coord + 2]
-        if (min > f_min):
-            min = f_min
-        if (max < f_max):
-            max = f_max
-    print(axis, "min, max : ", min, max)
-    return (min, max)
+    res_min = find_coord_list[0][coord]
+    res_max = find_coord_list[0][coord] + find_coord_list[0][coord + 2]
+    for i in range(len(find_coord_list)):
+        f_min = find_coord_list[i][coord]
+        f_max = find_coord_list[i][coord] + find_coord_list[i][coord + 2]
+        if res_min > f_min:
+            res_min = f_min
+        if res_max < f_max:
+            res_max = f_max
+    print(axis, "min, max : ", res_min, res_max)
+    return res_min, res_max
 
 # cv.namedWindow("Trackbar Windows", None)
 # cv.createTrackbar("threshold", "Trackbar Windows", 0, 255, onChange)
 # cv.setTrackbarPos("threshold", "Trackbar Windows", 180)
 
+
 image_origin = cv.imread("E:/workspace/tes_img/draw_to_image/test_img6.jpg", cv.IMREAD_COLOR)
 print(image_origin.shape)
 
 # while True:
-    # image_origin = cv.cvtColor(np.array(ImageGrab.grab()), cv.COLOR_BGR2RGB)
-new_image1 = np.zeros((image_origin.shape[0], image_origin.shape[1], 4), dtype=np.uint8) # np.zeros((height, width), np.uint8)
+# image_origin = cv.cvtColor(np.array(ImageGrab.grab()), cv.COLOR_BGR2RGB)
+# np.zeros((height, width), np.uint8)
+new_image1 = np.zeros((image_origin.shape[0], image_origin.shape[1], 4), dtype=np.uint8)
 image_resize = cv.resize(image_origin, dsize=(0, 0), fx=1, fy=1)
 img_gray = cv.cvtColor(image_resize, cv.COLOR_BGR2GRAY)
 img_blur = cv.bilateralFilter(img_gray, 9, 75, 75)
@@ -41,7 +44,7 @@ img_gray = cv.cvtColor(new_image1, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', img_gray)
 _, binary2 = cv.threshold(img_gray, 180, 255, cv.THRESH_BINARY)
 cv.imshow('binary2', binary2)
-contours2, hierarchy = cv.findContours(binary2, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+contours2, _ = cv.findContours(binary2, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
 find_coord = []
 for cnt in contours2:
@@ -54,9 +57,7 @@ for cnt in contours2:
 # find_min_max_coordinate(find_coord, 'x or y')
 # 1. find_coord : 찾은 직사각형의 x, y, w, h 값의 배열들
 # 2. x or y : 최소값과 최대값을 구할 축 x-min, x-max, y-min, y-max
-resolution = []
-resolution.append(find_min_max_coordinate(find_coord, 'x'))
-resolution.append(find_min_max_coordinate(find_coord, 'y'))
+resolution = [find_min_max_coordinate(find_coord, 'x'), find_min_max_coordinate(find_coord, 'y')]
 print(resolution)
 width = resolution[0][1] - resolution[0][0]
 height = resolution[1][1] - resolution[1][0]
@@ -69,4 +70,4 @@ cv.imshow("capture_window", image_resize)
 cv.imwrite("E:/workspace/tes_img/draw_to_image/test_cut_image.png", cp_img)
 
 key = cv.waitKey()
-cv.destroyWindow()
+cv.destroyWindow(0)
